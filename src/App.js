345953bf1bdda4c87fdf48307ca5c4c962787993
jaymhorsh <jaji.moshood97@yahoo.com>
2024-02-21 23:54:46 +0100

    SPA setup

--- src/App.js
+++ src/App.js
@@ -1,27 +1,5 @@
-import {useState } from "react";
-import React from "react";
-import Header from "./component/Layout/Header";
-import Meals from "./component/Meals/Meals";
-import Cart from "./component/Cart/Cart";
-import CartProvider from "./store/CartProvider";
-
 function App() {
-  const [cartIsShown, setCartIsShown] = useState(false);
-  const showCart = () => {
-    setCartIsShown(true);
-  };
-  const hideShowCart = () => {
-    setCartIsShown(false);
-  };
-  return (
-    <CartProvider>
-      <Header onShowCart={showCart} />
-      <main>
-        <Meals />
-        {cartIsShown && <Cart onClose={hideShowCart} />}
-      </main>
-    </CartProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
