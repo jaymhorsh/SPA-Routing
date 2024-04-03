42d3ae855383e315534e7c890993ea1cd496d31a
jaymhorsh <jaji.moshood97@yahoo.com>
2024-04-03 23:54:06 +0100

    updated SPA router project

--- src/routes.js
+++ src/routes.js
@@ -1,35 +1,62 @@
-// import React from 'react'
-import { useRoutes } from "react-router-dom";
-import RootLayout from "./pages/Root";
-import HomePage from "./pages/Home";
-import EventRoot from "./pages/EventRoot";
-import EventPage from "./pages/Event"; //{ loader as eventsLoader }
-import EventDetail from "./pages/EventDetail";
-import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
-import Error from "./pages/Error";
+import ErrorPage from "./pages/Error";
+import EventDetailPage, {
+  loader as eventDetailLoader,
+  action as deleteEventAction,
+} from "./pages/EventDetail";
+import EventsPage, { loader as eventsLoader } from "./pages/Events";
+import EventsRootLayout from "./pages/EventsRoot";
+import HomePage from "./pages/Home";
+import NewEventPage from "./pages/NewEvent";
+import RootLayout from "./pages/Root";
+import { action as manipulateEventAction } from "./components/EventForm";
+import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
 
 const Routes = () => {
   const routes = useRoutes([
     {
       path: "/",
       element: <RootLayout />,
-      errorElement: <Error />,
+      errorElement: <ErrorPage />,
       children: [
         { index: true, element: <HomePage /> },
         {
           path: "events",
-          element: <EventRoot />,
+          element: <EventsRootLayout />,
           children: [
             {
               index: true,
-              element: <EventPage />,
-              // loader: eventsLoader,
+              element: <EventsPage />,
+              loader: eventsLoader,
             },
-            { path: ":eventId", element: <EventDetail /> },
-            { path: "new", element: <NewEventPage /> },
-            { path: ":eventId/edit", element: <EditEventPage /> },
+            {
+              path: ":eventId",
+              id: "event-detail",
+              loader: eventDetailLoader,
+              children: [
+                {
+                  index: true,
+                  element: <EventDetailPage />,
+                  action: deleteEventAction,
+                },
+                {
+                  path: "edit",
+                  element: <EditEventPage />,
+                  action: manipulateEventAction,
+                },
+              ],
+            },
+            {
+              path: "new",
+              element: <NewEventPage />,
+              action: manipulateEventAction,
+            },
           ],
+        },
+        {
+          path: "newsletter",
+          element: <NewsletterPage />,
+          action: newsletterAction,
         },
       ],
     },
