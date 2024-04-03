2ef3822a3aee0bb26377977f6f2168caf9a5c3f1
jaymhorsh <jaji.moshood97@yahoo.com>
2024-04-04 00:23:51 +0100

    completed SPA router project

---
+++ src/pages/Events.js
@@ -0,0 +1,114 @@
+// import { Link } from "react-router-dom";
+
+// const DUMMY_EVENTS = [
+//   {
+//     id: "e1",
+//     title: "Some event",
+//   },
+//   {
+//     id: "e2",
+//     title: "Another event",
+//   },
+// ];
+
+// function EventsPage() {
+//   return (
+//     <>
+//       <h1>EventsPage</h1>
+//       <ul>
+//         {DUMMY_EVENTS.map((event) => (
+//           <li key={event.id}>
+//             <Link to={event.id}>{event.title}</Link>
+//           </li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// }
+
+// export default EventsPage;
+
+// import { useEffect, useState } from "react";
+
+// import EventsList from "../components/EventsList";
+
+// function EventsPage() {
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [fetchedEvents, setFetchedEvents] = useState();
+//   const [error, setError] = useState();
+
+//   useEffect(() => {
+//     // We can make the fetch data accessible to all sibling using the loader() from RRD
+//     async function fetchEvents() {
+//       setIsLoading(true);
+//       const response = await fetch("http://localhost:8080/events");
+
+//       if (!response.ok) {
+//         setError("Fetching events failed.");
+//       } else {
+//         const resData = await response.json();
+//         setFetchedEvents(resData.events);
+//       }
+//       setIsLoading(false);
+//     }
+
+//     fetchEvents();
+//   }, []);
+//   return (
+//     <>
+//       <div style={{ textAlign: "center" }}>
+//         {isLoading && <p>Loading...</p>}
+//         {error && <p>{error}</p>}
+//       </div>
+//       {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
+//     </>
+//   );
+// }
+// export default EventsPage;
+
+// We can make the fetch data accessible to all sibling using the loader() from react-router-dom
+
+import { Suspense } from 'react';
+import { useLoaderData, json, defer, Await } from 'react-router-dom';
+
+import EventsList from '../components/EventsList';
+
+function EventsPage() {
+  const { events } = useLoaderData();
+
+  return (
+    <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
+  );
+}
+
+export default EventsPage;
+
+async function loadEvents() {
+  const response = await fetch('http://localhost:8080/events');
+
+  if (!response.ok) {
+    // return { isError: true, message: 'Could not fetch events.' };
+    // throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
+    //   status: 500,
+    // });
+    throw json(
+      { message: 'Could not fetch events.' },
+      {
+        status: 500,
+      }
+    );
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
+
+export function loader() {
+  return defer({
+    events: loadEvents(),
+  });
+}
